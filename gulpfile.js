aed37935d6192cc20f33ebae7a11569d7ebe2681
Robert Gabriel <robert_gabriel@outlook.com>
2015-12-08 21:29:33 +0000

    Added gulp build to .min files

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,62 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var browserSync = require('browser-sync');
-var jshint = require('gulp-jshint');
 var gutil = require('gulp-util');
 var minifyCSS = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var chalk = require('chalk');
+var logger =  require('gulp-logger');
+var rename = require('gulp-rename');
 
 
 gulp.task('css', function () {
-    gulp.src('assets/css/*/*.css')
-        .pipe(minifyCSS({keepSpecialComments: 1})
+    gulp.src('assets/css/*/*')
+        .pipe(minifyCSS({keepSpecialComments: 1}))
+        .pipe(logger({
+            before: 'Compressing Css ',
+            after: 'Compressing finished!',
+            extname: '.min.css',
+            showChange: true
+        }))
+       .pipe(rename({
+            suffix: '.min'
+        }))
+
         .pipe(gulp.dest('./assets/dist/css'));
 })
 
 gulp.task('fonts', function () {
-    gulp.src('assets/fonts/*')
-        .pipe(minifyCSS({keepSpecialComments: 1})
+    gulp.src('assets/fonts/*.*')
+.pipe(logger({
+            before: 'Moving Fonts ',
+            after: 'Moving finished!',
+            extname: '',
+            showChange: true
+        }))
+
         .pipe(gulp.dest('./assets/dist/fonts'));
 })
 
 gulp.task('scripts', function () {
-    gulp.src('assets/js/*/*.js')
-        .pipe(uglify())
+    gulp.src('assets/js/*')
+        .pipe(uglify({
+       preserveComments:'all'
+     }))
+         .pipe(logger({
+            before: 'Starting Compressing Javascript',
+            after: 'Compressing complete!',
+            extname: '.js',
+            showChange: true
+        }))
+
+       .pipe(rename({
+            suffix: '.min'
+        }))
+
         .pipe(gulp.dest('./assets/dist/js'));
 })
 
 
-gulp.task('build', ['less','scripts']);
+gulp.task('build', ['css','fonts','scripts']);
 
 
 gulp.task('default', ['build']);
