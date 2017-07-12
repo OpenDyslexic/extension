41599697efc9b7b5cb068b050328e046ca2c4714
Sean O'Grady <sean@miralize.com>
2017-07-12 18:04:25 +0100

    reloading fixed

--- assets/js/app/core.js
+++ assets/js/app/core.js
@@ -31,7 +31,7 @@ window.onload = function() {
 				chrome.storage.sync.set({
 					"enabled": this.enabled
 				}, function() {
-					this.reloadPage();
+					self.reloadPage();
 				});
 			},
 			/*
